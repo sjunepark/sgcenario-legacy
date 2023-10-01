4b6033729dd7e82ba73e8b1213eb81ce95619da7
June Park <junepark202012@gmail.com>
2023-10-01 22:05:44 +0900

    Implement OverlayScrollbars to body

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script lang="ts">
 	import "../app.css";
+	import "overlayscrollbars/overlayscrollbars.css";
 	import NavHeader from "./NavHeader.svelte";
+	import { OverlayScrollbars } from "overlayscrollbars";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		OverlayScrollbars(document.body, {
+			scrollbars: {
+				visibility: "auto",
+				autoHide: "scroll",
+				autoHideDelay: 500,
+				autoHideSuspend: true
+			}
+		});
+	});
 </script>
 
-<div>
-	<NavHeader />
-	<main class="px-4 md:px-8 lg:px-12">
-		<slot />
-	</main>
-</div>
+<NavHeader />
+<main class="px-4 md:px-8 lg:px-12">
+	<slot />
+</main>
