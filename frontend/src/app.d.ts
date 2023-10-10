504a5f0808395af0fab61c311ea35e2c912193e3
June Park <junepark202012@gmail.com>
2023-10-10 20:34:14 +0900

    feature: set up lucia

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,11 +1,22 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+/// <reference types="lucia" />
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			auth: import("lucia").AuthRequest;
+		}
 		// interface PageData {}
 		// interface Platform {}
+	}
+
+	namespace Lucia {
+		type Auth = import("$lib/server/lucia").Auth;
+		/* eslint-disable @typescript-eslint/ban-types */
+		type DatabaseUserAttributes = {};
+		/* eslint-disable @typescript-eslint/ban-types */
+		type DatabaseSessionAttributes = {};
 	}
 }
 
