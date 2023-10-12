9037569e267aa67704d83d27f1146cfd691ade62
June Park <junepark202012@gmail.com>
2023-10-13 00:15:02 +0900

    refactor: remove backend folder

--- frontend/src/routes/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import type { LayoutServerLoad } from "./$types";
-
-// noinspection JSUnusedGlobalSymbols
-export const load: LayoutServerLoad = async ({ url, locals }) => {
-	const session = await locals.auth.validate();
-	return { pathname: url.pathname, user: session?.user };
-};
