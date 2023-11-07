274656609818fa8eac91eede39a21f8d5cc6e635
June Park <junepark202012@gmail.com>
2023-11-07 19:58:10 +0900

    feat: set up prose

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
-<div id="page" class="mx-auto w-fit bg-white p-16 shadow-md ring-1 ring-gray-500">
-	<article class="h-screen min-w-[50ch] max-w-[75ch]">HI</article>
+<div id="page" class="mx-auto w-fit bg-white p-16 shadow-md ring-1 ring-gray-500/50">
+	<article class="prose prose-lg h-screen min-w-[45ch] max-w-[75ch]">
+		<h1>Hi</h1>
+		<p>Hi</p>
+	</article>
 </div>
